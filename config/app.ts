5d23cfb4954c930597e4922780765b97dc02b948
Priyodarshi Ghosh <pghosh2002@yahoo.com>
2023-06-02 14:53:00 +0530

    changed url again

--- config/app.ts
+++ config/app.ts
@@ -4,7 +4,7 @@ export default registerAs('app', () => ({
   name: process.env.APP_NAME || 'NestJS App',
   env: process.env.APP_ENV || 'local',
   debug: +process.env.APP_DEBUG || 1,
-  url: '0.0.0.0',
+  url: process.env.APP_URL ||'localhost',
   port: +process.env.APP_PORT || 5005,
 }));
 //process.env.APP_URL || 'localhost'
