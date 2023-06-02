98a0d4999ffd183c7ff98c2c143727346b9d7b56
Priyodarshi Ghosh <pghosh2002@yahoo.com>
2023-06-02 15:53:11 +0530

    reverting the changes

--- config/app.ts
+++ config/app.ts
@@ -2,9 +2,9 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   name: process.env.APP_NAME || 'NestJS App',
-  env: 'deployment',
+  env: process.env.APP_ENV || 'local',
   debug: +process.env.APP_DEBUG || 1,
-  url: '0.0.0.0',
+  url: process.env.APP_URL ||'localhost',
   port: +process.env.APP_PORT || 5005,
 }));
 //process.env.APP_URL || 'localhost'
