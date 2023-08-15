61cd59e99fe8cea78bf323551e380edb3acd6af6
Priyodarshi Ghosh <pghosh2002@yahoo.com>
2023-08-15 17:27:17 +0530

    minor changes

--- config/database.ts
+++ config/database.ts
@@ -8,11 +8,11 @@ export default registerAs("db", () => ({
       client: 'mysql2',
       debug: !!+process.env.DB_DEBUG,
       connection: {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT ||3306,
-        database: process.env.DB_DATABASE||'job_portal',
-        user: process.env.DB_USER || 'aryan',
-        password: process.env.DB_PASSWORD ||'pabitra1234',
+        host: 'localhost',
+        port: 3306,
+        database: 'job_portal',
+        user:  'root',
+        password: 'pabitra1234',
         charset: 'utf8',
       },
         migrations: {
